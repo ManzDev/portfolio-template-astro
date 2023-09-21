1dbc1f48001afca047bc9454ca5f9c0926e380e7
ManzDev <JoseRomanHernandez@gmail.com>
2023-09-22 00:28:46 +0100

    Testimonials and index page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import svgSprite from "astro-svg-sprite";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://portfolio.manz.dev/",
+  integrations: [svgSprite()]
+});
